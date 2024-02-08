567a096e2354705182b59f5c8caa4b4d3246b86e
Pierre Mianes <pmianes@icloud.com>
2024-02-08 21:11:08 +0100

    feat: Ajout de la barre de filtre

--- FrontEnd/assets/Javascript/script.js
+++ FrontEnd/assets/Javascript/script.js
@@ -7,7 +7,7 @@ fetch('http://localhost:5678/api/works')
       imagesContainer.appendChild(figure);
     });
   });
-  
+
 const imagesContainer = document.querySelector('.gallery')
 
 function createWorkFigure(work) {
@@ -26,3 +26,100 @@ function createWorkFigure(work) {
 
   return figure;
 }
+
+/*PARTIE DES FILTRES*/
+
+/*fonction Objet*/
+
+function filtreObjet() {
+
+  const elements = document.querySelectorAll('div.gallery figure');
+  elements.forEach((element) => {
+    const categoryId = element.getAttribute('category-id')
+    if (categoryId === '1') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+
+var bouton = document.getElementById('boutonObjet');
+bouton.addEventListener('click',filtreObjet);
+
+
+/*fonction Appartements*/
+
+
+function filtreAppartements() {
+
+  const elements = document.querySelectorAll('div.gallery figure');
+  elements.forEach((element) => {
+    const categoryId = element.getAttribute('category-id')
+    if (categoryId === '2') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+
+var bouton = document.getElementById('boutonAppartement');
+bouton.addEventListener('click',filtreAppartements);
+
+
+/*fonction Hôtels & restaurants*/
+
+
+function filtreHotelsRestaurants() {
+
+  const elements = document.querySelectorAll('div.gallery figure');
+  elements.forEach((element) => {
+    const categoryId = element.getAttribute('category-id')
+    if (categoryId === '3') {
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
+    }
+  });
+}
+
+var bouton = document.getElementById('boutonHotelRestaurant');
+bouton.addEventListener('click',filtreHotelsRestaurants);
+
+
+/*fonction Tous*/
+
+
+function filtreTous() {
+
+  const elements = document.querySelectorAll('div.gallery figure');
+  elements.forEach((element) => {
+    element.style.display = 'block';
+  });
+}
+
+var bouton = document.getElementById('boutonTous');
+bouton.addEventListener('click',filtreTous);
+
+/* Fonction qui permet d'attribuer ou non la class selected*/
+
+const boutons = document.querySelectorAll('.filtre-bouton');
+
+boutons.forEach((bouton) => {
+    bouton.addEventListener('click', function() {
+      boutons.forEach((bouton) => {
+        bouton.classList.remove('selectione');
+      });
+      this.classList.add('selectione');
+      sessionStorage.setItem('boutonSelectionne', this.id);
+    });
+});
+ 
+/* Partie qui permet de revenir a "Tous" quand on recharge la page*/
+
+window.onbeforeunload = function(){
+sessionStorage.removeItem('boutonSelectionne');
+}
+
+/* PARTIE LOGIN */
